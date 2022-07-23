188584fa9c467f8199c27aec4392ea6b80dd5296
mvotho <jackfaulknerbis@hotmail.co.uk>
2022-07-23 12:50:29 +0100

    Implemented tmi.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import tmi from 'tmi.js';
 
 function App() {
+
+
+
+  const client = new tmi.Client({
+    channels: [ 'mvotho' ]
+  });
+  
+  client.connect();
+  
+  client.on('message', (channel, tags, message, self) => {
+    // "Alca: Hello, World!"
+    if(self || !message.startsWith('!q')) return;
+
+      console.log(`${tags['display-name']}: ${message}`);
+    
+  });
+      
+
 
   return (
     <div className='text-3xl font-bold underline'>
