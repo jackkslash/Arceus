74d603a7430931c2e6fcee8646a3b33ed810aee1
mvotho <jackfaulknerbis@hotmail.co.uk>
2022-07-26 19:34:03 +0100

    Added routing and restructed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,14 @@
-import { useEffect, useState } from 'react';
-import tmi from 'tmi.js';
-import { QuestionWidget } from './components/QuestionWidget';
-import { Question } from './types/types'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Main} from './pages/Main'; 
 
 
 function App() {
-
-  const [questions, setQuestions] = useState<String[]>([]);
-
-  useEffect(() => {
-
-    const client = new tmi.Client({
-      channels: ['mvotho']
-    });
-
-    client.connect();
-
-    client.on('message', (channel, tags, message, self) => {
-      if (self || !message.startsWith('!q')) return;
-
-      //console.log(`${tags['display-name']}: ${message}`);
-      const userId = tags['username'];
-      const userMessage = message;
-      if (message !== null) {
-        const uQuestion: Question = {
-          user: userId!,
-          question: userMessage
-        }
-
-        setQuestions((prevState: any) => [...prevState, uQuestion]);
-      }
-
-
-    });
-
-  }, [])
-
   return (
-    <>
-    <div className='h-64 text-3xl overflow-y-auto'>
-      {questions?.map((q:any) => (
-        // <p>{q.user}</p>
-        //   <p>{q.question}</p>
-        <QuestionWidget user={q.user} question={q.question}></QuestionWidget>
-      ))}
-    </div>
-  </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+      </Routes>
+    </Router>
     
   )
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { QuestionWidget } from '../components/QuestionWidget';
+import { Question } from '../types/types';
+import tmi from 'tmi.js';
+
+export const Main = () => {
+    const [questions, setQuestions] = useState<String[]>([]);
+
+    useEffect(() => {
+  
+      const client = new tmi.Client({
+        channels: ['mvotho']
+      });
+  
+      client.connect();
+  
+      client.on('message', (channel, tags, message, self) => {
+        if (self || !message.startsWith('!q')) return;
+  
+        //console.log(`${tags['display-name']}: ${message}`);
+        const userId = tags['username'];
+        const userMessage = message;
+        if (message !== null) {
+          const uQuestion: Question = {
+            user: userId!,
+            question: userMessage
+          }
+  
+          setQuestions((prevState: any) => [...prevState, uQuestion]);
+        }
+  
+  
+      });
+  
+    }, [])
+  
+    return (
+      <>
+      <div className='h-64 text-3xl overflow-y-auto'>
+        {questions?.map((q:any) => (
+          // <p>{q.user}</p>
+          //   <p>{q.question}</p>
+          <QuestionWidget user={q.user} question={q.question}></QuestionWidget>
+        ))}
+      </div>
+    </>
+      
+    )
+}
+
+export default Main;
